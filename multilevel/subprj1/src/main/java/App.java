c83312185b1900fa4e32d5d6a3fd4690da60530a
vinaykumar-rm <olivevinay@gmail.com>
2016-12-14 15:55:51 +0530

    v

---
+++ multilevel/subprj1/src/main/java/App.java
@@ -0,0 +1,15 @@
+/**
+ * Created by agarwals on 7/6/2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        String strOrig = "Hello readers";
+        int intIndex = strOrig.indexOf("Hello");
+        if(intIndex == - 1){
+            System.out.println("Hello not found");
+        }else{
+            System.out.println("Found Hello at index "
+                    + intIndex);
+        }
+    }
+}
---
+++ multilevel/subprj2/src/main/java/App2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by agarwals on 7/6/2016.
+ */
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Found Hello at index ");
+    }
+}
